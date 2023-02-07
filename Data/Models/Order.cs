153e54c864128a0dd45733df0983b7bcb20a8d87
Oshri Asulin <oshri12120@gmail.com>
2023-02-07 22:33:06 +0200

    add order class

--- Data/Models/Order.cs
+++ Data/Models/Order.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Data.Models
 {
-    internal class Order
+    public class Order
     {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public List<CartProduct>? Products { get; set; }
+
+        public int FinalPrice { get; set; }
+
+        public DateTime OrderTime { get; set; }
+
+        public Address Address { get; set; }
+
+        public CreditCard CreditCard { get; set; }
+        
+        public bool IsShipped { get; set; }
+
+        public bool Open { get; set; }
+
+      
     }
 }
